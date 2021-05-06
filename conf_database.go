6ddabb369f4510f3753ef8ecc1be5dac5259f519
R Andree <rxandree@gmail.com>
2021-05-06 13:25:03 +0200

    Conf files to root

---
+++ conf_database.go
@@ -0,0 +1,11 @@
+package multibase
+
+type DatabaseConf map[string]*ReplicationConf
+
+func NewDatabaseConf() DatabaseConf {
+	return make(DatabaseConf)
+}
+
+func (dbc DatabaseConf) AppendReplicationConf(name string, repConf *ReplicationConf) {
+	dbc[name] = repConf
+}
